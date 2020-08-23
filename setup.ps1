d2e8148106a07dda0ddcb702c834fbec10a340f3
Davvid <deivid.garcia.garcia@gmail.com>
2020-08-23 13:26:30 +0200

    Php74update (#1)
    Update to PHP 74
    Added test script (not working yet)

--- setup.ps1
+++ setup.ps1
@@ -1,26 +1,30 @@
-$location=Get-Location;
+param([string]$name="mypimcoresite")
+
+$location=(Get-Location).Path;
 
 # Deploy using Chef
-Invoke-ChefAppDeployPath $location mypimcoresite -Install;
+Invoke-ChefAppDeployPath $location $name -Install;
 
 # Bring in the application's php runtime
 $runtimePath = $(Invoke-ChefAppGetDeployment mypimcoresite).DeploymentActive.runtimePath;
 &"$runtimePath\include_path\setenv.ps1";
 
 #Remove symlnks and web.config before first install
+echo "Remove directory : " + "$location\mypimcoresite" 
 Remove-Item "$location\mypimcoresite" -Force -Recurse;
 
 # Install pimcore from composer
+Set-Location $location
 $Env:COMPOSER_MEMORY_LIMIT=-1;
-composer create-project pimcore/demo-ecommerce mypimcoresite;
+composer create-project pimcore/skeleton mypimcoresite;
 
 Copy-Item -Path "$location/web.config" -Destination "$location/mypimcoresite/web/web.config";
 
 # Redeploy to recover the symlinks
-Invoke-ChefAppRedeploy mypimcoresite -Force
+Invoke-ChefAppRedeploy $name -Force
 
 # Bring in the application's php runtime (changed after redeploy)
-$runtimePath = $(Invoke-ChefAppGetDeployment mypimcoresite).DeploymentActive.runtimePath;
+$runtimePath = $(Invoke-ChefAppGetDeployment $name).DeploymentActive.runtimePath;
 &"$runtimePath\include_path\setenv.ps1";
 
 # Run installer
